173f051590828cbff8f18b60644b972feaad0d78
Lance Thongsavanh <lancer8106@gmail.com>
2026-02-17 12:54:45 -0500

    Created dictionary of 4 unique usernames and passwords and a guest option.

--- Lab6_LanceTho-1.py
+++ Lab6_LanceTho-1.py
@@ -1,7 +1,23 @@
 """
-The program's name,
-Your name (the author),
-The purpose of the program,
-Any info about starter code (If used, where it came from, link, etc.), and the
-Date.
+Lab6_LanceTho-1.py
+Lance Thongsavanh
+Create a program that simulates a user login system using a dictionary to store usernames and passwords.
+2/17/2026
 """
+
+#Create a dictionary of at least four key-value pairs. Each key should be a unique username (string) and the associated value a password (string).
+login_system = {
+    "guest": "guest",
+    "JohnSmith": "password123",
+    "Subway": "E@tFr3sH",
+    "McDonalds": "ImL0v1ngiT",
+    "Nike": "jUstD01t"
+}
+
+#Ask the user to provide their username.
+#Check if the username is in the dictionary. If it is not, print an error and exit.
+#If the username is valid, ask the user to provide their password.
+#Check if the provided password matches the one stored in the dictionary for that user.
+#If the password is correct, assign a security level. #If the user is "guest", assign a "Guest" security level. For all other valid users, assign a "Security Level 1".
+#The program should end by printing a greeting to the username and letting them know their security level.
+#If the password is incorrect, print an "Access Denied" message and exit.
